657a0188a51a5d44e8b650b0d087c7d7644c89fc
Sumida Kazuhito <7437114+smichell@users.noreply.github.com>
2023-04-08 00:54:24 +0900

    Initial commit

---
+++ test/image-commentator-gpt-bot.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as ImageCommentatorGptBot from '../lib/image-commentator-gpt-bot-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/image-commentator-gpt-bot-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new ImageCommentatorGptBot.ImageCommentatorGptBotStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
